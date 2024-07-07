d4187869c8548eb0d8ee76e3fb8f5519c1beef10
arvee0220 <rowellverdejo@gmail.com>
2024-07-07 19:18:58 -0400

    Initial Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				ibmPlexSans: ["var(--font--ibmPlexSans)"],
+			},
+			backgroundImage: {
+				gradient: "url('/static/background.png')",
+			},
+			colors: {
+				purple: {
+					951: "rgba(79, 70, 229, 1)",
+				},
+				gray: {
+					950: "hsla(0, 0%, 100%, 0.7);",
+					951: "hsla(0, 0%, 100%, 0.4);",
+					952: "#373b64",
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
