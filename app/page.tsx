a27599f460ce6d42274b5c01153d0b5938c017a7
arvee0220 <rowellverdejo@gmail.com>
2024-07-07 19:50:43 -0400

    Update layout.tsx page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>Coffee Connoisseur</h1>
-			<h2>The best coffee shop in town</h2>
+			<div className="glass">
+				<h1>Coffee Connoisseur</h1>
+				<h2>The best coffee shop in town</h2>
+			</div>
 		</main>
 	);
 }
