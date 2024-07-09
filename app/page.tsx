9f47cd2ad3f539a1e606333a09406169de93e5a2
arvee0220 <rowellverdejo@gmail.com>
2024-07-09 06:59:33 -0400

    Create banner component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+import Banner from "@/components/banner.client";
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="glass">
-				<h1>Coffee Connoisseur</h1>
-				<h2>The best coffee shop in town</h2>
-			</div>
+			<Banner />
 		</main>
 	);
 }
